125a4d0abfbf061a9e4f62737d0f28ec35c4aacc
JuanM <juanmiguellp22@gmail.com>
2024-05-18 18:49:55 -0500

    Se cambia por el mapa probado en el local

--- src/pages/Ubicaciones/RCLinica.jsx
+++ src/pages/Ubicaciones/RCLinica.jsx
@@ -1,27 +1,54 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './a.css';
-import {
-  APIProvider,
-  Map,
-  AdvancedMarker,
-  Pin,
-  InfoWindow,
-} from "@vis.gl/react-google-maps";
 
 function RCLinica() {
-  const positon = {lat: 6.2476, lng: -75.5658};
-  const EIA = {lat: 6.1566, lng: -75.5176};
+  const mapRef = useRef(null); 
 
-  return (
-    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-      <div className='rutaClinica'>
-        <Map zoom={11} center={positon}>
-          <AdvancedMarker position={EIA}></AdvancedMarker>
-          <AdvancedMarker position={positon}></AdvancedMarker>
-        </Map>
-      </div>
-    </APIProvider>
-  );
+  useEffect(() => {
+    
+    const loadGoogleMapsScript = () => {
+      if (window.google) {
+        initializeMap();
+      } else {
+        const script = document.createElement('script');
+        script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}`;
+        script.async = true;
+        script.defer = true;
+        script.onload = () => initializeMap();
+        document.body.appendChild(script);
+      }
+    };
+
+    
+    const initializeMap = () => {
+      const map = new window.google.maps.Map(mapRef.current, {
+        zoom: 11,
+        center: { lat: 6.2476, lng: -75.5658 },
+      });
+
+     
+      const marker = new window.google.maps.Marker({
+        position: { lat: 6.158760098288912, lng: -75.51847096396926 },
+        map: map,
+        title: 'EIA'
+        
+      });
+    };
+
+    loadGoogleMapsScript();
+
+    
+    return () => {
+      const scripts = document.querySelectorAll('script');
+      for (let script of scripts) {
+        if (script.src.includes('maps.googleapis.com')) {
+          script.remove();
+        }
+      }
+    };
+  }, []);
+
+  return <div ref={mapRef} className="rutaClinica" />;
 }
 
 export default RCLinica;
