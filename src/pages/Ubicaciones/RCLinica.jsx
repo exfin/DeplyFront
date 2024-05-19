ab183edf2ac2b65b27bdba07dcbfb087a8ab22f0
JuanM <juanmiguellp22@gmail.com>
2024-05-18 20:24:09 -0500

    Prueba del metodo para agregar marcadores

--- src/pages/Ubicaciones/RCLinica.jsx
+++ src/pages/Ubicaciones/RCLinica.jsx
@@ -1,44 +1,62 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './a.css';
 
 function RCLinica() {
-  const mapRef = useRef(null); 
+  const mapRef = useRef(null); // Reference to the div element where the map will be
+  const map = useRef(null); // Reference to the Google Maps object
+  const markers = useRef([]); // Reference to the list of markers
 
   useEffect(() => {
-    
+    // Dynamically load the Google Maps script
     const loadGoogleMapsScript = () => {
-      if (window.google) {
+      if (window.google && mapRef.current && !map.current) {
         initializeMap();
-      } else {
+      } else if (!window.google) {
         const script = document.createElement('script');
         script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}`;
         script.async = true;
         script.defer = true;
-        script.onload = () => initializeMap();
+        script.onload = () => {
+          if (mapRef.current && !map.current) {
+            initializeMap();
+          }
+        };
         document.body.appendChild(script);
       }
     };
 
-    
+    // Initialize the map
     const initializeMap = () => {
-      const map = new window.google.maps.Map(mapRef.current, {
+      map.current = new window.google.maps.Map(mapRef.current, {
         zoom: 11,
         center: { lat: 6.2476, lng: -75.5658 },
       });
+    };
 
-     
-      const marker = new window.google.maps.Marker({
-        position: { lat: 6.158760098288912, lng: -75.51847096396926 },
-        map: map,
-        title: 'EIA'
-        
-      });
+    // Setup the SSE connection
+    const eventSource = new EventSource(import.meta.env.VITE_EVENT_SOURCE);
+    eventSource.onmessage = function(event) {
+      const data = JSON.parse(event.data);
+      addMarker(data);
     };
 
     loadGoogleMapsScript();
 
-    
+    // Function to add a marker to the map
+    const addMarker = (data) => {
+      if (data.latitude && data.longitude && map.current) {
+        const newMarker = new window.google.maps.Marker({
+          position: { lat: data.latitude, lng: data.longitude },
+          map: map.current,
+          title: data.title || 'New Marker'
+        });
+        markers.current.push(newMarker); // Keep track of the marker
+      }
+    };
+
+    // Clean up on component unmount
     return () => {
+      eventSource.close();
       const scripts = document.querySelectorAll('script');
       for (let script of scripts) {
         if (script.src.includes('maps.googleapis.com')) {
